306fad60d7f790cda2ce10821ad38fefd64684c7
paulo <paulo.m.campos6601@gmail.com>
2024-04-22 09:08:29 -0300

    Migrando para Spring

---
+++ project_bd/src/main/java/com/id/project_bd/ProjectBdApplication.java
@@ -0,0 +1,13 @@
+package com.id.project_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBdApplication.class, args);
+	}
+
+}
---
+++ project_bd/src/test/java/com/id/project_bd/ProjectBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.id.project_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
