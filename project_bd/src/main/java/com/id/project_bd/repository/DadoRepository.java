f94b68b16a6f8ca7e517bf04cbe7458d40536af6
paulo <paulo.m.campos6601@gmail.com>
2024-04-30 08:02:38 -0300

    Simplificação do POST na tabela dado

--- project_bd/src/main/java/com/id/project_bd/repository/DadoRepository.java
+++ project_bd/src/main/java/com/id/project_bd/repository/DadoRepository.java
@@ -15,20 +15,9 @@ public class DadoRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private boolean produtoExists(int id_produto) {
-        String sql = "SELECT COUNT(*) FROM PRODUTO WHERE ID_PRODUTO = ?";
-        int count = jdbcTemplate.queryForObject(sql, Integer.class, id_produto);
-        return count > 0;
-    }
-
     public void insertDado(Dado dado) {
-        // Verifica se o id_produto existe na tabela PRODUTO
-        if (produtoExists(dado.getId_produto())) {
-            jdbcTemplate.update("INSERT INTO DADO(FK_ID_PRODUTO, QTD_LADOS) VALUES (?, ?)",
-                    dado.getFk_id_produto(), dado.getQtd_lados());
-        } else {
-            throw new RuntimeException("ID_PRODUTO não encontrado na tabela PRODUTO.");
-        }
+        jdbcTemplate.update("INSERT INTO DADO(FK_ID_PRODUTO, QTD_LADOS) VALUES(?, ?)", 
+            dado.getFk_id_produto(), dado.getQtd_lados());
     }
 
     public boolean deleteDado(int fk_id_produto) {
