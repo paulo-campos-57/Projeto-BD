2a6390391fd63ff3e54069f9c9cf213ffe4cade7
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2024-05-02 18:44:52 -0300

    finalizando tabela JOGADOR

--- project_bd/src/main/java/com/id/project_bd/controllers/JogadorController.java
+++ project_bd/src/main/java/com/id/project_bd/controllers/JogadorController.java
@@ -1,6 +1,11 @@
 package com.id.project_bd.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +26,21 @@ public class JogadorController {
         jogadorRepository.insertJogador(jogador);
         return "Jogador inserido!\n";
     }
+
+    @DeleteMapping("/{fk_id_user}")
+    public String deletejogador(@PathVariable int fk_id_user){
+        boolean deleted = jogadorRepository.deleteJogador(fk_id_user);
+        if(deleted){
+            return "Jogador deletado!\n";
+        } else {
+            return "O jogador com essas informações não existe no banco de dados!\n";
+        }
+    }
+
+    @GetMapping
+    public List<Jogador> getJogador(){
+        return jogadorRepository.getAllJogadores();
+    }
+
+    //Não criei PUT de jogador pois ele não tem nada pra mudar
 }
--- project_bd/src/main/java/com/id/project_bd/repository/JogadorRepository.java
+++ project_bd/src/main/java/com/id/project_bd/repository/JogadorRepository.java
@@ -1,5 +1,7 @@
 package com.id.project_bd.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,5 +18,18 @@ public class JogadorRepository {
         jdbcTemplate.update("INSERT INTO JOGADOR(FK_ID_USER) VALUES(?)", jogador.getFk_id_user());
         System.out.println("to aq");
         System.out.println(jogador.getFk_id_user());
+    }
+
+    public boolean deleteJogador(int fk_id_user){
+        int rowsAffected = jdbcTemplate.update("DELETE FROM JOGADOR WHERE FK_ID_USER = ?", fk_id_user);
+        return rowsAffected > 0;
+    }
+
+    public List<Jogador> getAllJogadores(){
+        return jdbcTemplate.query("SELECT * FROM JOGADOR", (resultSet, rowNum) -> {
+            Jogador jogador = new Jogador();
+            jogador.setFk_id_user(resultSet.getInt("FK_ID_USER"));
+            return jogador;
+        });
     }
 }
