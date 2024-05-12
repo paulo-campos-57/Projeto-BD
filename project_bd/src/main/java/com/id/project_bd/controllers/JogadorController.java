af01ca7eed9a46d2bbac6f5350f0e47b866dc976
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2024-05-12 10:59:48 -0300

    Tabela Jogador feito

--- project_bd/src/main/java/com/id/project_bd/controllers/JogadorController.java
+++ project_bd/src/main/java/com/id/project_bd/controllers/JogadorController.java
@@ -3,13 +3,16 @@ package com.id.project_bd.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.id.project_bd.models.Jogador;
 import com.id.project_bd.repository.JogadorRepository;
@@ -20,6 +23,36 @@ public class JogadorController {
 
     @Autowired
     private JogadorRepository jogadorRepository;
+
+    @RequestMapping(value = "/cadastro/{id_user}", method = RequestMethod.GET)
+    public ModelAndView jogadorForm(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("jogadorForm");
+        return mv;
+    }
+
+    @RequestMapping(value = "/cadastro/{id_user}", method = RequestMethod.POST)
+    public ModelAndView jogadorForm(Jogador jogador){
+        jogadorRepository.insertJogador(jogador);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+
+    @GetMapping("/lista")
+    public ModelAndView getAllJogadores(){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("jogador", jogadorRepository.getAllJogadores());
+        mv.setViewName("listaJogador");
+        return mv;
+    }
+
+    @GetMapping("/excluir/{fk_id_user}")
+    public String excluirJogador(@PathVariable("fk_id_user") Integer fk_id_user, Model model){
+        jogadorRepository.deleteJogador(fk_id_user);
+        model.addAttribute("jogador", jogadorRepository.getAllJogadores());
+        return "listaJogador";
+    }
 
     @PostMapping
     public String createJogador(@RequestBody Jogador jogador){
