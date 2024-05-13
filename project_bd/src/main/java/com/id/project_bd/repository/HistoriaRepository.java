2d2f0382bf033970a3d832d44c12a6a0bb12aab3
paulo <paulo.m.campos6601@gmail.com>
2024-05-13 07:05:34 -0300

    Normalização de front

--- project_bd/src/main/java/com/id/project_bd/repository/HistoriaRepository.java
+++ project_bd/src/main/java/com/id/project_bd/repository/HistoriaRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.id.project_bd.models.Historia;
-import com.id.project_bd.models.Mestre;
 
 @Repository
 public class HistoriaRepository {
@@ -17,22 +16,23 @@ public class HistoriaRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void insertHistoria(Historia historia){
-        jdbcTemplate.update("INSERT INTO HISTORIA(NOME, PROLOGO, QTD_JOGADORES, DT_INICIO, PRESENCIAL, FK_ID_MESTRE) VALUES(?, ?, ?, ?, ?, ?)", 
-        historia.getNome(), historia.getPrologo(), historia.getQtd_jogadores(), historia.getDt_inicio(), historia.isPresencial(), historia.getFk_id_mestre());
+    public void insertHistoria(Historia historia) {
+        jdbcTemplate.update(
+                "INSERT INTO HISTORIA(NOME, PROLOGO, QTD_JOGADORES, DT_INICIO, PRESENCIAL, FK_ID_MESTRE) VALUES(?, ?, ?, ?, ?, ?)",
+                historia.getNome(), historia.getPrologo(), historia.getQtd_jogadores(), historia.getDt_inicio(),
+                historia.isPresencial(), historia.getFk_id_mestre());
         historia.setId_historia(historia.getId_historia());
     }
 
-    public boolean deleteHistoria(int id_historia){
+    public boolean deleteHistoria(int id_historia) {
 
         jdbcTemplate.update("DELETE FROM participacao WHERE FK_ID_HISTORIA = ?", id_historia);
-    
 
         int rowsAffected = jdbcTemplate.update("DELETE FROM HISTORIA WHERE ID_HISTORIA = ?", id_historia);
         return rowsAffected > 0;
     }
 
-    public List<Historia> getAllHistoria(){
+    public List<Historia> getAllHistoria() {
         return jdbcTemplate.query("SELECT * FROM HISTORIA", (resultSet, rowNum) -> {
             Historia historia = new Historia();
             historia.setId_historia(resultSet.getInt("ID_HISTORIA"));
@@ -47,17 +47,20 @@ public class HistoriaRepository {
     }
 
     @SuppressWarnings("deprecation")
-    public Historia getHistoriaById(int id_historia){
+    public Historia getHistoriaById(int id_historia) {
         String sql = "SELECT * FROM HISTORIA WHERE ID_HISTORIA = ?";
-        try{
-            return jdbcTemplate.queryForObject(sql, new Object[]{id_historia}, new BeanPropertyRowMapper<>(Historia.class));
-        } catch (EmptyResultDataAccessException e){
+        try {
+            return jdbcTemplate.queryForObject(sql, new Object[] { id_historia },
+                    new BeanPropertyRowMapper<>(Historia.class));
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
 
-    public void updateHistoria(Historia historia){
-        jdbcTemplate.update("UPDATE HISTORIA SET NOME = ?, PROLOGO = ?, QTD_JOGADORES = ?, PRESENCIAL = ? WHERE ID_HISTORIA = ?", 
-        historia.getNome(), historia.getPrologo(), historia.getQtd_jogadores(), historia.isPresencial(), historia.getId_historia());
+    public void updateHistoria(Historia historia) {
+        jdbcTemplate.update(
+                "UPDATE HISTORIA SET NOME = ?, PROLOGO = ?, QTD_JOGADORES = ?, PRESENCIAL = ? WHERE ID_HISTORIA = ?",
+                historia.getNome(), historia.getPrologo(), historia.getQtd_jogadores(), historia.isPresencial(),
+                historia.getId_historia());
     }
 }
