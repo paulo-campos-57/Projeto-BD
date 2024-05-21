eec017f3a4ac568330f5f3ef9406eaa13fd97b8b
paulo <paulo.m.campos6601@gmail.com>
2024-05-21 12:09:23 -0300

    Features de front end

--- project_bd/src/main/java/com/id/project_bd/controllers/ParticipacaoController.java
+++ project_bd/src/main/java/com/id/project_bd/controllers/ParticipacaoController.java
@@ -3,9 +3,7 @@ package com.id.project_bd.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
