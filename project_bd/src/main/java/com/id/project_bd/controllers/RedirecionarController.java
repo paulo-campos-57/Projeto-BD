ddb5dd38528b65256931e9ccd480f492388fced6
Gabriel Rossiter <gabrielrossiter04@hotmail.com>
2024-05-22 22:34:07 -0300

    adicionando redireciona para historia

--- project_bd/src/main/java/com/id/project_bd/controllers/RedirecionarController.java
+++ project_bd/src/main/java/com/id/project_bd/controllers/RedirecionarController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.id.project_bd.models.Historia;
 import com.id.project_bd.models.User;
+import com.id.project_bd.repository.HistoriaRepository;
 import com.id.project_bd.repository.UserRepository;
 
 @RestController
@@ -17,12 +19,24 @@ public class RedirecionarController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/{id_user}")
-    public ModelAndView redirecionar(@PathVariable("id_user") int id_user) {
+    @Autowired
+    private HistoriaRepository historiaRepository;
+
+    @GetMapping("/usuario/{id_user}")
+    public ModelAndView redirecionarUsuario(@PathVariable("id_user") int id_user) {
         User user = userRepository.getUserById(id_user);
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("redirecionar");
+        mv.setViewName("redirecionarUsuario");
         mv.addObject("user", user);
+        return mv;
+    }
+
+    @GetMapping("/historia/{id_historia}")
+    public ModelAndView redirecionarHIstoria(@PathVariable("id_historia") int id_historia){
+        Historia historia = historiaRepository.getHistoriaById(id_historia);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirecionarHisoria");
+        mv.addObject("historia", historia);
         return mv;
     }
 }
